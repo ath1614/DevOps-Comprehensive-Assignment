4c9f81c87455327ec4feafccb8f63ae735500b1b
ath1614 <atharv.soni@adypu.edu.in>
2026-04-26 10:36:18 +0530

    Hello

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"))
+})
+
 app.use("/api/users", userRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 
